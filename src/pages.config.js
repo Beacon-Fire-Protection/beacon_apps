63574c431d88e4bc0bd1a7ff00e31c418ba5114b
Johnson Ecommerce LLC <johnsonecommercellc@gmail.com>
2025-12-02 03:14:20 +0000

    Static Screenshot Solutions

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,12 +1,10 @@
 import Home from './pages/Home';
 import MyWork from './pages/MyWork';
-import AdminScreenshots from './pages/AdminScreenshots';
 
 
 export const PAGES = {
     "Home": Home,
     "MyWork": MyWork,
-    "AdminScreenshots": AdminScreenshots,
 }
 
 export const pagesConfig = {
